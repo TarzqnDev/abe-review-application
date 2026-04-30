acbedb3331993fe961c096e5296beb0ffd9633a8
TarzqnDev <mjjjantework@gmail.com>
2026-04-30 14:39:18 +0800

    Added MCQ Content Placeholder

--- app/admin/mcqcontent/page.tsx
+++ app/admin/mcqcontent/page.tsx
@@ -1,7 +1,50 @@
 export default function AdminMCQContentPage() {
   return (
-    <div>
-      <h1>MCQ Content</h1>
-    </div>
+    <section>
+      <div className="mb-6">
+        <h1 className="font-semibold text-2xl">MCQ Content Management</h1>
+        <p className="text-lg text-stone-600">
+          Manage MCQ content for the learning platform.
+        </p>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="w-full">
+          <div className="bg-teal-500 h-40 flex items-center p-6 rounded-t-2xl">
+            <h1 className="font-semibold text-xl text-white">Guess the Word</h1>
+          </div>
+          <div className="flex flex-col justify-between h-50 border border-gray-300 p-6 rounded-b-2xl">
+            <p>Majo</p>
+            <button className="bg-teal-800 text-white py-2 px-4 rounded cursor-pointer">
+              Try
+            </button>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="bg-teal-500 h-40 flex items-center p-6 rounded-t-2xl">
+            <h1 className="font-semibold text-xl text-white">AB-Solution</h1>
+          </div>
+          <div className="flex flex-col justify-between h-50 border border-gray-300 p-6 rounded-b-2xl">
+            <p>Majo</p>
+            <button className="bg-teal-800 text-white py-2 px-4 rounded cursor-pointer">
+              Try
+            </button>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="bg-teal-500 h-40 flex items-center p-6 rounded-t-2xl">
+            <h1 className="font-semibold text-xl text-white">Situationship</h1>
+          </div>
+          <div className="flex flex-col justify-between h-50 border border-gray-300 p-6 rounded-b-2xl">
+            <p>Majo</p>
+            <button className="bg-teal-800 text-white py-2 px-4 rounded cursor-pointer">
+              Try
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- components/AdminHeader.tsx
+++ components/AdminHeader.tsx
@@ -1,22 +1,33 @@
 import { useAdminDashboard } from "@/features/admin/dashboard/hooks/useAdminDashboard";
 import { Cog6ToothIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export default function AdminHeader() {
   const { handleLogout } = useAdminDashboard();
 
   const [openAccountMenu, setOpenAccountMenu] = useState(false);
+  const pathname = usePathname();
+
+  const pageMap: Record<string, string> = {
+    "/admin/dashboard": "Dashboard",
+    "/admin/mcqcontent": "MCQ Content",
+  };
+
+  const pageName = pageMap[pathname] || "Dashboard";
 
   return (
     <div className="mt-4 flex justify-between items-center mb-10">
       <div>
         <p>
-          <span className="text-stone-600">Pages</span> / Dashboard
+          <span className="text-stone-600">Pages</span> / {pageName}
         </p>
       </div>
 
       <div className="flex gap-6 relative">
-        <Cog6ToothIcon className="h-6 w-6 text-stone-600" />
+        <button className="cursor-pointer">
+          <Cog6ToothIcon className="h-6 w-6 text-stone-600" />
+        </button>
         <button
           className="cursor-pointer"
           onClick={() => setOpenAccountMenu(!openAccountMenu)}
@@ -28,12 +39,10 @@ export default function AdminHeader() {
           <div className="absolute right-0 mt-8 w-48 bg-white border border-gray-300 rounded-md shadow-sm">
             <ul className="py-2">
               <li className="px-4 py-2 hover:bg-stone-100 cursor-pointer">
-                Profile
+                <button>Profile</button>
               </li>
               <li className="px-4 py-2 hover:bg-stone-100 cursor-pointer">
-                <button className="w-full text-start" onClick={handleLogout}>
-                  Logout
-                </button>
+                <button onClick={handleLogout}>Logout</button>
               </li>
             </ul>
           </div>
